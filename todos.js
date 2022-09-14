5c591e1156fb7fd84ac8bc1e6bc38a05f9dd3793
jokay03J <55850775+Jokay03J@users.noreply.github.com>
2022-09-14 14:13:33 +0200

    fix incorrect renderer

--- todos.js
+++ todos.js
@@ -8,18 +8,23 @@ export const setupTodos = ({ input, todos, done, todo, send, all }) => {
     todosList.push({ title: input.value, checked: false });
     input.value = "";
 
+    renderList(todosList, todos);
+
     todosList.map((todo, index) => {
-      todos.innerHTML += `
-            <li class="todos">
-                <input type="checkbox" id="todo-${index}" >
-                <label for="todo-${index}">${todo.title}</label>
-            </li>
-            `;
-      document.getElementById(`todo-${index}`).addEventListener("click", () => {
-        todosList[index].checked = !todosList[index].checked;
-        console.log(todosList);
-      });
+      // let id = index;
+      // todos.innerHTML += `
+      //       <li class="todos">
+      //           <input type="checkbox" id="todo-${id}" >
+      //           <label for="todo-${id}">${todo.title}</label>
+      //       </li>
+      //       `;
+      // document.getElementById(`todo-${id}`).addEventListener("click", () => {
+      //   todosList[id].checked = !todosList[id].checked;
+      //   // console.log(todosList[id]);
+      //   console.log(todosList);
+      // });
     });
+    console.log(todosList);
   });
 
   done.addEventListener("click", () => {
@@ -44,9 +49,15 @@ function renderList(todosList,todos_container) {
         .map((todo, index) => {
         todos_container.innerHTML += `
                 <li class="todos">
-                    <input type="checkbox" id="todo-${index}" >
+                    <input type="checkbox" id="todo-${index}" ${todosList[index].checked ? "checked" : ""}>
                     <label for="todo-${index}">${todo.title}</label>
                 </li>
                 `;
         });
+      for(let i = 0; i < todosList.length; i++) {
+        document.getElementById(`todo-${i}`).addEventListener("click", () => {
+          todosList[i].checked = !todosList[i].checked;
+          console.log(todosList[i]);
+        });
+      }
 }
